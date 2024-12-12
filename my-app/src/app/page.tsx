30cdd43e3b034f561bd92739622e4920ace2d973
codersoumya007 <soumyapanda87631@gmail.com>
2024-12-12 11:10:51 +0530

    update

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Calendar } from '../components/Calendar'
 import { EventModal } from '../components/EventModal'
 import { EventList } from '../components/EventList'
 import { EventFilter } from '../components/EventFilter'
-import { useCalendar,Event } from '../contexts/CalendarContext'
+import { useCalendar } from '../contexts/CalendarContext'
 import { Button } from '@/components/ui/button'
 import { format } from 'date-fns'
 
